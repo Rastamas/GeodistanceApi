b0d22a4353ea7b05752dec1f66267e6a13056a0a
Tamás Szabó <r.tamas.szabo@gmail.com>
2022-06-29 09:25:24 +0200

    Add simple integration test from API call

--- GeodistanceApi/GeodistanceApi/Program.cs
+++ GeodistanceApi/GeodistanceApi/Program.cs
@@ -1,28 +1,34 @@
 using GeodistanceApi.Services;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddSingleton<IGeoDistanceSerice, GeoDistanceService>();
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+public partial class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddSingleton<IGeoDistanceSerice, GeoDistanceService>();
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
---
+++ GeodistanceApi/IntegrationTests/GeoDistanceApiTests.cs
@@ -0,0 +1,42 @@
+using GeodistanceApi.Models;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Net.Http;
+using System.Net.Mime;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace IntegrationTests
+{
+    public class GeoDistanceApiTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+
+        public GeoDistanceApiTests(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task GetDistancePositive()
+        {
+            const double expectedValue = 82.95;
+            var requestBody = JsonSerializer.Serialize(new GeoDistanceRequest
+            {
+                From = new Coordinates(46.8964, 19.6897),
+                To = new Coordinates(47.4979, 19.0402)
+            });
+
+            var client = _factory.CreateClient();
+
+            var response = await client.PostAsync("/GeoDistance", new StringContent(requestBody, Encoding.UTF8, "application/json"));
+
+            response.EnsureSuccessStatusCode();
+            var responseValue = double.Parse(await response.Content.ReadAsStringAsync());
+
+            Assert.Equal(expectedValue, Math.Round(responseValue, 2));
+        }
+    }
+}
